7fb4f55eb971fbdcf6598c643932879180d6a57c
eddi <evillegas@globalstd.com>
2019-10-23 17:20:28 -0500

    feature: Implementacion de variables de entorno
    Se crearon variables de entorno para el proyecto con un archivo .env

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const path = require('path')
 const indexRouters = require('./routes/index')
-const port = process.env.PORT || 3000;
+require('dotenv').config()
+
+const port = process.env.PORT
 const app = express();
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
 app.set ('views', path.join(__dirname, 'views'));
+
 app.set('view engine', 'pug');
 
 app.use('/', indexRouters)
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const route = express.Router();
+const {Router} = require('express')
+const route = Router();
 const indexCtrl = require('../controllers/index')
 
 route.get('/', indexCtrl.index)
